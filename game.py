add0b2ee927495054e612384f0b82e06795f9b8f
Roman1456 <151393377+Roman1456@users.noreply.github.com>
2024-06-09 19:35:42 +0300

    proekt4

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import *
 from Um_yum import Um_yum
+from Сandies import Сandies
 
 window = pygame.display.set_mode((700,500))
 fps = pygame.time.Clock()
@@ -9,10 +10,10 @@ backround = pygame.transform.scale(
     pygame.image.load("kartunka/fon.jpg"),(700,500)
 )
 
-um_yum = Um_yum(50,50,100,100, "kartunka/download.jpg",10)
+um_yum = Um_yum(280,360,100,100, "kartunka/player.png",10)
 
-
-
+candies1 = []
+candies1.append(Сandies(50,50,100,50,"kartunka/candies.png",5))
 
 
 game = True
@@ -26,7 +27,17 @@ while game:
 
 
     window.blit(backround, (0, 0))
-    um_yum.move()
-    um_yum.draw(window)
+    for candies in candies1:
+        candies.draw(window)
+        candies.move()
+
+        um_yum.move()
+        um_yum.draw(window)
+
+    if um_yum.hitbox.colliderect(candies.hitbox):
+        pygame.display.flip()
+
+
+
     pygame.display.flip()
     fps.tick(60)
