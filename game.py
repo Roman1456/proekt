f2972632439d1e058f94635b2f98531d052a75ac
Roma <stadnykroman2010@gmail.com>
2024-06-22 19:25:25 +0300

    proekt7

--- game.py
+++ game.py
@@ -5,63 +5,65 @@ from random import *
 from Um_yum import Um_yum
 from Сandies import Сandies
 
-pygame.init()
-pygame.font.init()
-window = pygame.display.set_mode((700,500))
-fps = pygame.time.Clock()
+def game():
+    pygame.init()
+    pygame.font.init()
+    window = pygame.display.set_mode((700,500))
+    fps = pygame.time.Clock()
 
-backround = pygame.transform.scale(
-    pygame.image.load("kartunka/fon.jpg"),(700,500)
-)
+    backround = pygame.transform.scale(
+        pygame.image.load("kartunka/fon.jpg"),(700,500)
+    )
 
-points = 0
+    points = 0
 
-um_yum = Um_yum(280,360,100,100, "kartunka/player.png",10)
+    um_yum = Um_yum(280,360,100,100, "kartunka/player.png",10)
 
-candies1 = []
-candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
-candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
-candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
-candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
-
-
-font = pygame.font.Font(None, 36)
-
-game = True
-while game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
-
-    text_surface = font.render("Рахунок :", True, (0, 0, 0))
-    text_surface1 = font.render(str(points), True, (0, 0, 0))
-    window.blit(text_surface, (20, 20))
-    window.blit(text_surface1, (140, 20))
-    pygame.display.flip()
-    fps.tick(60)
+    candies1 = []
+    candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
+    candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
+    candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
+    candies1.append(Сandies(randint(0, 450),randint(-500,0),100,50,"kartunka/candies.png",5))
 
 
-    window.blit(backround, (0, 0))
-    um_yum.move()
-    um_yum.draw(window)
+    font = pygame.font.Font(None, 36)
 
-    for candies in candies1:
-        candies.draw(window)
-        candies.move()
+    game = True
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())
+
+        text_surface = font.render("Рахунок :", True, (0, 0, 0))
+        text_surface1 = font.render(str(points), True, (0, 0, 0))
+        window.blit(text_surface, (20, 20))
+        window.blit(text_surface1, (140, 20))
+        pygame.display.flip()
+        fps.tick(60)
 
 
+        window.blit(backround, (0, 0))
+        um_yum.move()
+        um_yum.draw(window)
 
-        if um_yum.hitbox.colliderect(candies.hitbox):
-            candies.hitbox.x = randint(0, 450)
-            candies.hitbox.y = randint(-500,0)
-            points+=1
-            pygame.display.flip()
-            break
+        for candies in candies1:
+            candies.draw(window)
+            candies.move()
 
 
 
-    pygame.display.flip()
-    fps.tick(60)
+            if um_yum.hitbox.colliderect(candies.hitbox):
+                candies.hitbox.x = randint(0, 450)
+                candies.hitbox.y = randint(-500,0)
+                points+=1
+                pygame.display.flip()
+                break
+
+
+
+        pygame.display.flip()
+        fps.tick(60)
--- menu.py
+++ menu.py
@@ -1,4 +1,7 @@
 from PyQt5.QtWidgets import *
+import main
+import game
+import qest_window
 
 app = QApplication([])
 
@@ -12,7 +15,9 @@ menu_qest_btn1 = QPushButton("Грати")
 menu_qest_btn2 = QPushButton("Магазин")
 menu_qest_btn3 = QPushButton("Інветрарь")
 
+
 mine_line = QVBoxLayout()
+
 
 h1 = QHBoxLayout()
 h1.addWidget(menu_qest_btn1)
@@ -29,7 +34,20 @@ mine_line.addLayout(h3)
 
 
 
+def menu_show():
+    window.hide()
+    game.game()
+    window.show()
 
+def menu_show1():
+    window.hide()
+    main.shop()
+    window.show()
+
+
+
+menu_qest_btn2.clicked.connect(menu_show1)
+menu_qest_btn1.clicked.connect(menu_show)
 window.setLayout(mine_line)
 window.show()
 app.exec()
